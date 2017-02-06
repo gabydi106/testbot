7770c7f9405b658379160489daeab6149a2e78d0
gabydi106 <gabydi106@gmail.com>
2017-02-06 21:28:29 +0800

    fix infinite loop

--- index.js
+++ index.js
@@ -32,13 +32,13 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-          
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
 			
 			if (text === 'Generic') {
                 sendGenericMessage(event.sender.id)
                 continue
             }
+			
+			sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
         }
     }
     res.sendStatus(200);
